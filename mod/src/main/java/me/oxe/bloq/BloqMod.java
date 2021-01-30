d02f4a4ea0a1bbd958bbc1632894e19ea74ac8f5
Ethan Smith <ethan@ethanhs.me>
2021-01-30 15:18:07 -0800

    Remove Mixin

--- mod/src/main/java/me/oxe/bloq/BloqMod.java
+++ mod/src/main/java/me/oxe/bloq/BloqMod.java
@@ -5,10 +5,6 @@ import net.fabricmc.api.ModInitializer;
 public class BloqMod implements ModInitializer {
   @Override
   public void onInitialize() {
-    // This code runs as soon as Minecraft is in a mod-load-ready state.
-    // However, some things (like resources) may still be uninitialized.
-    // Proceed with mild caution.
-
     System.out.println("Hello Fabric world!");
   }
 }
