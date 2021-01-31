884597809e3a020d1eed1ad29a5132f0eb4cc166
Marc Davis <marc.davis@berkeley.edu>
2021-01-30 22:26:21 -0500

    added qubit item

--- mod/src/main/java/me/oxe/bloq/BloqMod.java
+++ mod/src/main/java/me/oxe/bloq/BloqMod.java
@@ -1,17 +1,30 @@
 package me.oxe.bloq;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class BloqMod implements ModInitializer {
   public static final Block X_GATE_BLOCK = new XGateBlock(FabricBlockSettings.of(Material.METAL).strength(4.0f));
   public static BlockEntityType<XGateBlockEntity> X_GATE_BLOCK_ENTITY;
+  public static final ItemQubit ITEM_QUBIT = new ItemQubit(new FabricItemSettings().maxCount(1));
+  public static final ItemGroup QUANTUM_GROUP = FabricItemGroupBuilder.create(new Identifier("bloq","quantum")).icon(() -> new ItemStack(ITEM_QUBIT))
+  .appendItems(stacks -> {
+    stacks.add(new ItemStack(ITEM_QUBIT));
+  }).build();
 
   @Override
   public void onInitialize() {
-    System.out.println("Hello Fabric world!");
+    System.out.println("|Hello> + |Goodbye> World!");
+    Registry.register(Registry.ITEM, new Identifier("bloq", "qubit"), ITEM_QUBIT);
   }
 }
---
+++ mod/src/main/java/me/oxe/bloq/ItemQubit.java
@@ -0,0 +1,31 @@
+package me.oxe.bloq;
+
+import java.util.List;
+import java.util.UUID;
+
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.text.Text;
+import net.minecraft.world.World;
+
+public class ItemQubit extends Item {
+
+    public ItemQubit(Settings settings) {
+        super(settings);
+        // TODO Auto-generated constructor stub
+    }
+
+
+
+    @Override
+    public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
+        CompoundTag tag = itemStack.getOrCreateTag();
+        if (!tag.containsUuid("QUBIT_UUID")) {
+            tag.putUuid("QUBIT_UUID", UUID.randomUUID());
+        }
+        tooltip.add(Text.of(tag.getUuid("QUBIT_UUID").toString()));
+    }
+}
